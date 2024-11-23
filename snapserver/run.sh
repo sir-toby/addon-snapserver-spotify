5383add2c5d79c5408990ea616e0c130c0239d25
sir-toby <sir-toby@users.noreply.github.com>
2024-11-23 08:51:58 +0100

    updaterun.sh to start server and client

--- snapserver/run.sh
+++ snapserver/run.sh
@@ -1,85 +1,16 @@
-#!/usr/bin/env bashio
+#!/usr/bin/env bash
 
-#mkdir -p /share/snapfifo
+# Configuration generation
+bashio::log.info "Generating SnapServer configuration..."
+config="/etc/snapserver.conf"
 
-if [ "$(bashio::config 'spotify.bypass_apresolve')" = true ]; then
-    echo "0.0.0.0	apresolve.spotify.com" >> /etc/hosts
-fi
+# Stream
+echo "[stream]" > "${config}"
 
-config=/etc/snapserver.conf
-spotify_credentials_file=$(bashio::config 'spotify.cache_dir')/credentials.json
-
-if ! [ -d $(bashio::config 'spotify.cache_dir') ]; then
-    mkdir $(bashio::config 'spotify.cache_dir')
-fi
-
-if ! bashio::fs.file_exists '/etc/snapserver.conf'; then
-    touch /etc/snapserver.conf ||
-        bashio::exit.nok "Could not create snapserver.conf file on filesystem"
-fi
-bashio::log.info "Populating snapserver.conf..."
-
-# Server Config
-echo "[server]" > "${config}"
-
-# Threads
-if bashio::config.has_value 'server.threads'; then
-    echo "threads = $(bashio::config 'server.threads')" >> "${config}"
-fi
-
-# Datadir
-if bashio::config.has_value 'server.datadir'; then
-    echo "datadir = $(bashio::config 'server.datadir')" >> "${config}"
-fi
-
-# Stream Config
-echo "[stream]" >> "${config}"
-
-# Spotify
-echo $(bashio::config 'spotify.credentials') > ${spotify_credentials_file}
-echo -n "stream = spotify:///librespot?name=Spotify" >> "${config}"
-echo -n "&devicename=$(bashio::config 'spotify.device_name')" >> "${config}"
-echo -n "&bitrate=$(bashio::config 'spotify.bitrate')" >> "${config}"
-echo -n "&volume=$(bashio::config 'spotify.volume')" >> "${config}"
-echo -n "&cache=$(bashio::config 'spotify.cache_dir')" >> "${config}"
-echo -n "&autoplay=true" >> "${config}"
-#echo -n "&wd_timeout=3600" >> "${config}" # This might lead to uneccesary restarts if there are no log lines by librespot
-echo -n "&params=--disable-discovery%20--cache-size-limit%3D$(bashio::config 'spotify.cache_size_limit')" >> "${config}"
-if bashio::config 'spotify.normalize' == true; then
-    echo -n "&normalize=true" >> "${config}"
-fi
-
-echo "" >> "${config}"
-
-# Other streams
-if bashio::config.has_value 'stream.streams'; then
-    echo "stream = $(bashio::config 'stream.streams')" >> "${config}"
-fi
-
-# Stream bis and ter # it was in the fork, idk wtf it's for
-if bashio::config.has_value 'stream.bis'; then
-    echo "$(bashio::config 'stream.bis')" >> "${config}"
-fi
-if bashio::config.has_value 'stream.ter'; then
-    echo "$(bashio::config 'stream.ter')" >> "${config}"
-fi
-
-# Buffer
-if bashio::config.has_value 'stream.buffer'; then
-    echo "buffer = $(bashio::config 'stream.buffer')" >> "${config}"
-fi
-
-# Codec
 if bashio::config.has_value 'stream.codec'; then
     echo "codec = $(bashio::config 'stream.codec')" >> "${config}"
 fi
 
-# Muted
-if bashio::config.has_value 'stream.send_to_muted'; then
-    echo "send_to_muted = $(bashio::config 'stream.send_to_muted')" >> "${config}"
-fi
-
-# Sampleformat
 if bashio::config.has_value 'stream.sampleformat'; then
     echo "sampleformat = $(bashio::config 'stream.sampleformat')" >> "${config}"
 fi
@@ -112,9 +43,12 @@ if bashio::config.has_value 'logging.enabled'; then
     echo "debug = $(bashio::config 'logging.enabled')" >> "${config}"
 fi
 
-
+# Start services
 bashio::log.info "Starting SnapServer..."
-exec snapserver
+snapserver &  # Run snapserver in the background
 
 bashio::log.info "Starting SnapClient..."
-exec snapclient -h $(bashio::config 'host') -p $(bashio::config 'port')
+snapclient -h $(bashio::config 'host') -p $(bashio::config 'port') &  # Run snapclient in the background
+
+# Wait for all background processes to finish
+wait
